9360f7de04d6b4f39810f2fd8f207e1b7f8daf57
Evreu1pro <57584813+Evreu1pro@users.noreply.github.com>
2020-04-04 16:33:29 +0300

    gfsgsg

---
+++ task4/index.js
@@ -0,0 +1,9 @@
+let year = prompt('вікові групи', '');
+
+if (year < 16) {
+  alert( 'діти ' );
+} else if (year < 60) {
+  alert( 'дорослі' );
+}else if (year < 100) {
+    alert( 'пенсіонери' );
+}
